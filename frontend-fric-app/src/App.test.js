be78c93f5c39b12d11f5a4a8cc4cd65de7591fab
samarjorg <Sftslth99>
2020-10-12 02:53:47 -0600

    react-gentella basic template from Nearsoft repo

--- frontend-fric-app/src/App.test.js
+++ frontend-fric-app/src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/* global it */
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import AppFactory from './js/App';
+import history from './js/history';
+import { EmptyComponent } from './testUtils';
+
+const App = AppFactory(React, Router, history, EmptyComponent, EmptyComponent, EmptyComponent, EmptyComponent, EmptyComponent);
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App/>, div);
 });
