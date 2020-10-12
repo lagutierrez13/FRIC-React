6ff7e062a3abb143d57e3fcd9c799bc101e97c35
Luis Gutierrez <luisgtzh18@gmail.com>
2020-10-12 12:09:00 -0500

    Removed unused icons sidebar footer

--- frontend-fric-app/src/js/navigation/Left.js
+++ frontend-fric-app/src/js/navigation/Left.js
@@ -129,15 +129,6 @@ class Left extends Component {
 
           { /*/menu footer buttons*/ }
           <div className="sidebar-footer hidden-small">
-            <a data-toggle="tooltip" data-placement="top" title="Settings">
-              <span className="glyphicon glyphicon-cog" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-              <span className="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="Lock">
-              <span className="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-            </a>
             <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
               <span className="glyphicon glyphicon-off" aria-hidden="true"></span>
             </a>
