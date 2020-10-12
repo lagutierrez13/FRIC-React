eeb602947c90069d4e0bf1708adce27aa4b60fa7
Luis Gutierrez <luisgtzh18@gmail.com>
2020-10-12 12:06:27 -0500

    Removed unnecessary files

--- frontend-fric-app/src/js/routes/configuration/index.js
+++ frontend-fric-app/src/js/routes/configuration/index.js
@@ -4,11 +4,6 @@ import Dashboard from './dashboard-1/Dashboard'
 import ConfigurationMenu from './Menu'
 
 export default [
-    /*<Route path="/configuration/dashboard-1" component={Dashboard} />,*/
-    
-    /**
-     * this is where each of the pages will be routed, the "component" is the type of view
-     */
     <Route path="/configuration/configuration" component={Dashboard} />, 
     <Route path="/configuration/setup" component={Dashboard} />,
     <Route path="/configuration/help" component={Dashboard} />
