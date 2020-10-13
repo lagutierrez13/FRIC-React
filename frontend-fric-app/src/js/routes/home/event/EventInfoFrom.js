ca116eb9ba11b5093fe34ebd387f9dfaf8e345f9
Victor Fernandez <vmfernandez4@miners.utep.edu>
2020-10-13 00:36:40 -0600

    events front end almos done

--- frontend-fric-app/src/js/routes/home/event/EventInfoFrom.js
+++ frontend-fric-app/src/js/routes/home/event/EventInfoFrom.js
@@ -5,31 +5,30 @@ class DetailedView extends Component {
     return (
       // x_panel is container
       <div class="x_panel">
-        <div class="x_tittle">
+        <div class="x_title">
           <h2>Event Information</h2>
+          <div class="clearfix"></div>
         </div>
-        <div class="x_content ">
-          <form class="form-horizontal form-label-left">
+        <div class="x_content">
+          <form>
             {/* Event name */}
-            <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">Event Name</label>
-              <div class="col-md-9 col-sm-9 ">
+            <div class="form-group row">
+              <label class="control-label  col-sm-2 ">Event Name</label>
+              <div class=" col-sm-10 ">
                 <input type="text" class="form-control" />
               </div>
             </div>
             {/* Event Description */}
-            <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">
-                Event Description
-              </label>
-              <div class="col-md-9 col-sm-9 ">
+            <div class="form-group row">
+              <label class="control-label col-sm-2">Event Description</label>
+              <div class=" col-sm-10">
                 <textarea class="form-control" rows="4"></textarea>
               </div>
             </div>
             {/* event type */}
             <div class="form-group row">
-              <label class="control-label col-md-3 col-sm-3 ">Event Type</label>
-              <div class="col-md-9 col-sm-9 ">
+              <label class="control-label col-md-2 col-sm-2 ">Event Type</label>
+              <div class="col-md-10 col-sm-10 ">
                 <select class="form-control">
                   <option>Choose option</option>
                   <option>Option one</option>
@@ -41,10 +40,10 @@ class DetailedView extends Component {
             </div>
             {/* Event Version */}
             <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">
+              <label class="control-label col-md-2 col-sm-2 ">
                 Event Version
               </label>
-              <div class="col-md-9 col-sm-9 ">
+              <div class="col-md-10 col-sm-10 ">
                 <input type="text" class="form-control" />
               </div>
             </div>
@@ -52,26 +51,26 @@ class DetailedView extends Component {
 
             {/* Organization Name */}
             <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">
+              <label class="control-label col-md-2 col-sm-2 ">
                 Organization Name
               </label>
-              <div class="col-md-9 col-sm-9 ">
+              <div class="col-md-10 col-sm-10 ">
                 <input type="text" class="form-control" />
               </div>
             </div>
             {/*  Security Classification Title Guide*/}
             <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">
+              <label class="control-label col-md-2 col-sm-2 ">
                 Security Classification Title Guide
               </label>
-              <div class="col-md-9 col-sm-9 ">
+              <div class="col-md-10 col-sm-10 ">
                 <input type="text" class="form-control" />
               </div>
             </div>
             {/* Event Classificatrion */}
             <div class="form-group row">
-              <label class="control-label col-md-3 col-sm-3 ">Event Type</label>
-              <div class="col-md-9 col-sm-9 ">
+              <label class="control-label col-md-2 col-sm-2 ">Event Type</label>
+              <div class="col-md-10 col-sm-10 ">
                 <select class="form-control">
                   <option>Choose option</option>
                   <option>Option one</option>
@@ -84,10 +83,10 @@ class DetailedView extends Component {
             {/* Declassification Date */}
             {/* Customer Name */}
             <div class="form-group row ">
-              <label class="control-label col-md-3 col-sm-3 ">
+              <label class="control-label col-md-2 col-sm-2 ">
                 Customer Name
               </label>
-              <div class="col-md-9 col-sm-9 ">
+              <div class="col-md-10 col-sm-10 ">
                 <input type="text" class="form-control" />
               </div>
             </div>
--- frontend-fric-app/src/js/routes/home/event/EventView.js
+++ frontend-fric-app/src/js/routes/home/event/EventView.js
@@ -3,6 +3,7 @@ import { Row, Col, Clearfix } from "react-bootstrap";
 import { XPanel, PageTitle } from "../../../components";
 import DetailedView from "./EventInfoFrom";
 import EventTable from "./EventTable";
+import TeamInfo from "./EventTeamInfo";
 
 class EventView extends Component {
   constructor(props) {
@@ -43,6 +44,16 @@ class EventView extends Component {
               <XPanel.Title title="Event Detailed View"></XPanel.Title>
               <XPanel.Content>
                 <DetailedView />
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} sm={12} xs={12}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="Event Team Information"></XPanel.Title>
+              <XPanel.Content>
+                <TeamInfo />
               </XPanel.Content>
             </XPanel>
           </Col>
