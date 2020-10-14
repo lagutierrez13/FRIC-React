342f1b41b08b7cb48f9570a020806b6a1454f56e
Victor Fernandez <vmfernandez4@miners.utep.edu>
2020-10-14 01:22:55 -0600

    Systems frontend almost done

---
+++ frontend-fric-app/src/js/routes/home/System/SystemView.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Row, Col, Clearfix } from "react-bootstrap";
+import { XPanel, PageTitle } from "../../../components";
+import SystemTable from "./SystemTable";
+import DetailedView from "./SystemInfoForm";
+import Categorization from "./SystemCategorization";
+
+class SystemView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      panelVisible: true,
+    };
+  }
+
+  render() {
+    const { panelVisible } = this.state;
+    const onHide = (e) => this.setState({ panelVisible: !panelVisible });
+
+    return (
+      <div>
+        <PageTitle
+          showSearch
+          title="System"
+          onSearch={(text) => console.log(text)}
+        />
+        <Clearfix />
+        <Row>
+          <Col md={12} sm={12} xs={12}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="System Table"></XPanel.Title>
+              <XPanel.Content>
+                <SystemTable />
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} sm={12} xs={12}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="System Detailed View"></XPanel.Title>
+              <XPanel.Content>
+                <DetailedView />
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} sm={12} xs={12}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="System Categorization"></XPanel.Title>
+              <XPanel.Content>
+                <Categorization />
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default SystemView;
