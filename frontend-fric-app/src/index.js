2a20eb45f17e7aaf70373177a9e20528b19db796
Luis Gutierrez <luisgtzh18@gmail.com>
2020-10-25 01:13:47 -0500

    Research login stuff

--- frontend-fric-app/src/index.js
+++ frontend-fric-app/src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/custom.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/custom.css";
 // import registerServiceWorker from './registerServiceWorker';
 
-import { Left, Top, Footer } from './js/navigation';
-import history from './js/history';
-import { Router } from 'react-router';
-import PageContent, { Menu } from './js/routes';
+import { Left, Top, Footer } from "./js/navigation";
+import history from "./js/history";
+import { Router } from "react-router";
+import PageContent, { Menu } from "./js/routes";
 
-import AppFactory from './js/App';
-const App = AppFactory(React, Router, history, Left, Top, Footer, PageContent, Menu);
+import AppFactory from "./js/App";
+const App = AppFactory(
+  React,
+  Router,
+  history,
+  Left,
+  Top,
+  Footer,
+  PageContent,
+  Menu
+);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 // registerServiceWorker();
