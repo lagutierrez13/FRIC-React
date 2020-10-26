7e2ab70771b912684a6f365bdacb5ae194bdddee
Victor Fernandez <vmfernandez4@miners.utep.edu>
2020-10-26 01:02:41 -0600

    task, subtasks tables updates

--- frontend-fric-app/src/js/routes/home/Task/TaskTable.js
+++ frontend-fric-app/src/js/routes/home/Task/TaskTable.js
@@ -2,10 +2,44 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+const Task = (props) => (
+  <tr>
+    <td>{props.task.task_title}</td>
+    <td>{props.task.system}</td>
+    <td>{props.task.working_analyst}</td>
+    <td>{props.task.priority}</td>
+    <td>{props.task.task_progress}</td>
+    <td>{props.task.no_of_subtasks}</td>
+    <td>{props.task.no_of_findings}</td>
+    <td>{props.task.due_date}</td>
+    <td></td>
+    <td>
+      <Link to={"/update/" + props.task._id}>Edit</Link>
+    </td>
+  </tr>
+);
+
 class TaskTable extends Component {
   constructor(props) {
     super(props);
     this.state = { tasks: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/home/tasks/get")
+      .then((response) => {
+        this.setState({ tasks: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  taskList() {
+    return this.state.tasks.map(function (currentTask, i) {
+      return <Task task={currentTask} key={i} />;
+    });
   }
 
   render() {
