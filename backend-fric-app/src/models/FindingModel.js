918833dfc49e813cda088604d84f14acbf95081b
Luis Gutierrez <luisgtzh18@gmail.com>
2020-10-28 19:29:23 -0500

    Fix finding progress

--- backend-fric-app/src/models/FindingModel.js
+++ backend-fric-app/src/models/FindingModel.js
@@ -16,7 +16,7 @@ const findingSchema = new Schema(
       classification: String,
       type: String,
       risk: String,
-      prpgress: Number,
+      progress: Number,
       // slug: {
       //   type: String,
       //   required: true,
