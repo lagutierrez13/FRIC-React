9ea4dce35d38e61b4ff034950bb2e20074731740
nicoletorres <nicolevardo@gmail.com>
2020-11-07 23:58:42 -0700

    Added circular event progress bars [FINAL] to AnalystProgressView

--- frontend-fric-app/src/js/routes/home/Analyst/AnalystProgressView.js
+++ frontend-fric-app/src/js/routes/home/Analyst/AnalystProgressView.js
@@ -3,12 +3,11 @@ import { Row, Col, Clearfix } from "react-bootstrap";
 import { XPanel, PageTitle } from "../../../components";
 import TaskTable from "../Task/TaskTable";
 import SubtaskTable from "../Subtask/SubtaskTable";
-import FindingOverviewTable from "../findings/FindingOverviewTable";
 import SystemTable from "../System/SystemTable";
 import FindingTable from "../finding/FindingTable";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import {value} from "mongoose";
+
 
 class AnalystProgressView extends Component {
   constructor(props) {
@@ -22,30 +21,73 @@ class AnalystProgressView extends Component {
     const { panelVisible } = this.state;
     const onHide = (e) => this.setState({ panelVisible: !panelVisible });
     const percentage = 40;
+    const percentage2 = 50;
+    const percentage3 = 70;
+    const percentage4 = 90;
 
     return (
 
       <div>
-
         <PageTitle
           showSearch
           onSearch={(text) => console.log(text)}
           title="Analyst Progress View"
           onSearch={(text) => console.log(text)}
         />
-        <Row>
-          <Col md={12} sm={12} xs={12}>
+
+
+
+          <Col md={3} sm={3} xs={3}>
             <XPanel visible={panelVisible} onHide={onHide}>
               <XPanel.Title title="Event Progress"></XPanel.Title>
               <XPanel.Content>
-                <div style={{ width: 300, height: 300}}>
-                  <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+                <div style={{width: 200, height: 200}}>
+                  <CircularProgressbar value={percentage} text={`${percentage}%`} />
                 </div>
 
               </XPanel.Content>
             </XPanel>
           </Col>
-        </Row>
+
+        <Col md={3} sm={3} xs={3}>
+          <XPanel visible={panelVisible} onHide={onHide}>
+            <XPanel.Title title="Task Progress"></XPanel.Title>
+            <XPanel.Content>
+              <div style={{width: 200, height: 200}}>
+                <CircularProgressbar value={percentage2} text={`${percentage2}%`} />
+              </div>
+
+            </XPanel.Content>
+          </XPanel>
+        </Col>
+
+
+
+          <Col md={3} sm={3} xs={3}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="Subtask Progress"></XPanel.Title>
+              <XPanel.Content>
+                <div style={{width: 200, height: 200}}>
+                  <CircularProgressbar value={percentage3} text={`${percentage3}%`} />
+                </div>
+
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+
+
+          <Col md={3} sm={3} xs={3}>
+            <XPanel visible={panelVisible} onHide={onHide}>
+              <XPanel.Title title="Systems Progress"></XPanel.Title>
+              <XPanel.Content>
+                <div style={{width: 200, height: 200}}>
+                  <CircularProgressbar value={percentage4} text={`${percentage4}%`} />
+                </div>
+
+              </XPanel.Content>
+            </XPanel>
+          </Col>
+
 
 
 
